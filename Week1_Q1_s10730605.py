73e12e2dbf4ec2a272f89de3a8ba1d5c4f4759e9
QAQOuOTAT <161003276+QAQOuOTAT@users.noreply.github.com>
2024-02-29 09:35:51 +0800

    Add files via upload

--- Week1_Q1_s10730605.py
+++ Week1_Q1_s10730605.py
@@ -8,11 +8,6 @@ This is a temporary script file.
 a=float(input('input a:'))
 b=float(input('input b:'))
 c=float(input('input c:'))
-x=b**2-4*a*c
-if x>0:
-    print('have 2 different root')
-elif x<0:
-    print('have no root')
-else:
-    print('have 1 real root')
-    
+x1=(-b+((b**2)-4*a*c)**0.5)/(2*a)
+x2=(-b-((b**2)-4*a*c)**0.5)/(2*a)
+print(x1,x2)
